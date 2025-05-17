b379797843b7fdde2de957b6fb262bedf3640f93
Ser <ser@serbook.local>
2025-05-17 08:19:05 +0200

    finita introduzione udp

--- Client.fsx
+++ Client.fsx
@@ -1,52 +1,24 @@
 #nowarn "9"
 
+open System.Net
 open System.Text
-open System.Text.Json
 open System.Net.Sockets
-open System
-open Microsoft.FSharp.NativeInterop
-
-let ask prompt =
-    printf "%A\n> " prompt
-    Console.ReadLine() |> fun s -> s.Trim()
 
 let main () =
-    let firstNumber = ask "Inserisci il primo numero"
+    use client = new UdpClient 1200
+    client.Client.ReceiveTimeout <- 3000
+    let serverEndpoint = new IPEndPoint(IPAddress.Loopback, 1100)
 
-    if firstNumber |> Int32.TryParse |> fst |> not then
-        failwith "Non un numero!"
+    let data = Encoding.ASCII.GetBytes "ping"
 
-    let secondNumber = ask "Inserisci il secondo numbero"
+    printfn "[C] Sending data..."
 
-    if secondNumber |> Int32.TryParse |> fst |> not then
-        failwith "Non un numero!"
+    client.Connect serverEndpoint
+    let _ = client.Send data
 
-    let operation = ask "Inserisci un'operazione (+, -, *, /)"
+    printfn "[C] Data sent! Waiting for response..."
 
-    let server = new TcpClient("127.0.0.1", 5005)
-    let stream = server.GetStream()
-
-    {| firstNumber = int firstNumber
-       secondNumber = int secondNumber
-       operation = operation |}
-    |> JsonSerializer.Serialize
-    |> Encoding.ASCII.GetBytes
-    |> stream.Write
-
-    printfn "[C] Bytes written!"
-
-    let mem = NativePtr.stackalloc<byte> (256 * 4) |> NativePtr.toVoidPtr
-    let mutable span = Span<byte>(mem, 256 * 4)
-
-    let mutable shouldReceive = true
-
-    while shouldReceive do
-        if stream.Read span = 0 then
-            shouldReceive <- false
-
-    let response = Encoding.ASCII.GetString span
-
-    printfn $"[C] Server resp: <{response}>"
-
+    let resp = client.Receive(ref serverEndpoint)
+    resp |> Encoding.ASCII.GetString |> printfn "[C] %A"
 
 main ()
--- Program.fs
+++ Program.fs
@@ -1,74 +1,23 @@
 ﻿#nowarn "9"
 
+open System
 open System.Net
-open System.Net.Sockets
 open System.Text
-open System.Text.Json
-
-[<Struct>]
-type Operation =
-    | Sum
-    | Sub
-    | Prod
-    | Div
-    | Invalid
-
-    static member fromStr =
-        function
-        | "+" -> Sum
-        | "-" -> Sub
-        | "*" -> Prod
-        | "/" -> Div
-        | _ -> Invalid
-
-[<Struct>]
-type Request =
-    { firstNumber: int
-      secondNumber: int
-      operation: string }
-
+open System.Net.Sockets
 
 [<EntryPoint>]
 let main _ =
-    let ip = IPAddress.Parse "127.0.0.1"
-    use server = new TcpListener(ip, 5005)
+    use server = new UdpClient 1100
 
-    printfn "[S] Calcolatrice, %A" server.LocalEndpoint
-    server.Start()
+    printfn "[S] Listening..."
 
-    while true do
-        use client = server.AcceptTcpClient()
-        use clientStream = client.GetStream()
+    let endpoint = IPEndPoint(IPAddress.Loopback, 1200)
+    let res = server.Receive(ref endpoint)
+    server.Connect endpoint
 
-        let mutable respBytes = Array.zeroCreate 256
+    res |> Encoding.ASCII.GetString |> printfn "[S] %A"
 
-        let _ = clientStream.Read(respBytes, 0, respBytes.Length)
-
-        let msg =
-            Encoding.ASCII.GetString respBytes //
-            |> Seq.filter (fun t -> t <> '\000')
-            |> Seq.toArray
-
-        let data = JsonSerializer.Deserialize<Request> msg
-
-        let result =
-            match Operation.fromStr data.operation with
-            | Invalid -> "Operazione invalida!"
-            | valid ->
-                (match valid with
-                 | Sum -> (+)
-                 | Sub -> (-)
-                 | Prod -> (*)
-                 | Div -> (/)
-                 | _ -> failwith "Impossible case")
-                |> fun op -> op data.firstNumber data.secondNumber
-                |> string
-
-        $"[S] Risultato: {result}" //
-        |> Encoding.ASCII.GetBytes
-        |> clientStream.Write
-
-
-    server.Stop()
+    let data = Encoding.ASCII.GetBytes "pong"
+    let _ = server.Send(data, data.Length)
 
     0
